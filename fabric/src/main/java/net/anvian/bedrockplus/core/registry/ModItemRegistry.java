5adc275043b857322390ca07581740501a2956e5
anviaan <andre.carbajal.vargas@gmail.com>
2024-07-16 14:27:28 -0500

    Multiloader 1.21

--- fabric/src/main/java/net/anvian/bedrockplus/core/registry/ModItemRegistry.java
+++ fabric/src/main/java/net/anvian/bedrockplus/core/registry/ModItemRegistry.java
@@ -8,22 +8,22 @@ import net.minecraft.resources.ResourceLocation;
 
 public class ModItemRegistry {
     public static void registerItems() {
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impure_bedrock"), ModItems.IMPURE_BEDROCK);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impure_bedrock_block"), ModItems.IMPURE_BEDROCK_BLOCK);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impure_bedrock"), ModItems.IMPURE_BEDROCK);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impure_bedrock_block"), ModItems.IMPURE_BEDROCK_BLOCK);
 
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impure_bedrock_ingot"), ModItems.IMPURE_BEDROCK_INGOT);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impure_bedrock_scrap"), ModItems.IMPURE_BEDROCK_SCRAP);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impure_bedrock_upgrade_smithing_template"), ModItems.IMPURE_BEDROCK_UPGRADE_SMITHING_TEMPLATE);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impure_bedrock_ingot"), ModItems.IMPURE_BEDROCK_INGOT);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impure_bedrock_scrap"), ModItems.IMPURE_BEDROCK_SCRAP);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impure_bedrock_upgrade_smithing_template"), ModItems.IMPURE_BEDROCK_UPGRADE_SMITHING_TEMPLATE);
 
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_sword"), ModItems.IMPUREBEDROCK_SWORD);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_pickaxe"), ModItems.IMPUREBEDROCK_PICKAXE);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_axe"), ModItems.IMPUREBEDROCK_AXE);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_shovel"), ModItems.IMPUREBEDROCK_SHOVEL);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_hoe"), ModItems.IMPUREBEDROCK_HOE);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_sword"), ModItems.IMPUREBEDROCK_SWORD);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_pickaxe"), ModItems.IMPUREBEDROCK_PICKAXE);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_axe"), ModItems.IMPUREBEDROCK_AXE);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_shovel"), ModItems.IMPUREBEDROCK_SHOVEL);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_hoe"), ModItems.IMPUREBEDROCK_HOE);
 
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_helmet"), ModItems.IMPURE_BEDROCK_HELMET);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_chestplate"), ModItems.IMPURE_BEDROCK_CHESTPLATE);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_leggings"), ModItems.IMPURE_BEDROCK_LEGGINGS);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Constants.MOD_ID, "impurebedrock_boots"), ModItems.IMPURE_BEDROCK_BOOTS);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_helmet"), ModItems.IMPURE_BEDROCK_HELMET);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_chestplate"), ModItems.IMPURE_BEDROCK_CHESTPLATE);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_leggings"), ModItems.IMPURE_BEDROCK_LEGGINGS);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "impurebedrock_boots"), ModItems.IMPURE_BEDROCK_BOOTS);
     }
 }
